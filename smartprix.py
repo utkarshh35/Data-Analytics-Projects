1a5bdab1888e4bfc6733c7f0d7571a0bf3c78264
Utkarsh Chauhan Singh <107413564+utkarshh35@users.noreply.github.com>
2024-01-25 09:48:25 +0530

    Update smartprix.py

--- smartprix.py
+++ smartprix.py
@@ -1,3 +1,5 @@
+# Code in Pycharm:
+
 import time
 
 from selenium import webdriver
@@ -38,3 +40,111 @@ html = driver.page_source
 with open('smartprix.html','w',encoding='utf-8') as f:
     f.write(html)
 
+# Code in Jupiter Notebook:
+
+with open('smartprix.html','r',encoding='utf-8') as f:
+    html = f.read()
+
+from bs4 import BeautifulSoup 
+import numpy as np 
+import pandas as pd
+
+soup = BeautifulSoup(html, 'lxml')
+
+containers = soup.find_all('div', {'class': 'sm-product has-tag has-features has-actions'})
+
+names = []
+prices = []
+ratings = []
+sim = []
+processor = []
+ram = []
+battery = []
+display = []
+camera = []
+card = []
+os = []
+
+for i in soup.find_all('div',{'class':'sm-product has-tag has-features has-actions'}):
+    try:
+        names.append(i.find('h2').text)
+    except:
+        names.append(np.nan)
+    try:
+        prices.append(i.find('span',{'class':'price'}).text)
+    except:
+        price.append(np.nan)
+    try:
+        ratings.append(i.find('div',{'class':'score rank-2-bg'}).find('b').text)
+    except:
+        ratings.append(np.nan)
+        
+    x = i.find('ul',{'class':'sm-feat specs'}).find_all('li')
+    try:
+        sim.append(x[0].text)
+    except:
+        sim.append(np.nan)
+    try:
+        processor.append(x[1].text)
+    except:
+        processor.append(np.nan)
+    try:    
+        ram.append(x[2].text)
+    except:
+        ram.append(np.nan)
+    try:
+        battery.append(x[3].text)
+    except:
+        battery.append(np.nan)
+    try:
+        display.append(x[4].text)
+    except:
+        display.append(np.nan)
+    try:
+        camera.append(x[5].text)
+    except:
+        camera.append(np.nan)
+    try:
+        card.append(x[6].text)
+    except:
+        card.append(np.nan)
+    try:
+        os.append(x[7].text)
+    except:
+        os.append(np.nan)
+
+df = pd.DataFrame({
+    'model': names,
+    'price': prices,
+    'rating': ratings,
+    'sim': sim,
+    'processor': processor,
+    'ram': ram,
+    'battery': battery,
+    'display': display,
+    'camera': camera,
+    'card': card,
+    'os': os
+})
+
+df.isnull().sum()
+
+df = pd.DataFrame({
+    'model': names,
+    'price': prices,
+    'rating': ratings,
+})
+
+df = pd.DataFrame({
+    'model': names,
+    'price': prices,
+    'rating': ratings,
+    'sim': sim,
+    'processor': processor,
+    'ram': ram,
+    'battery': battery,
+    'display': display,
+    'camera': camera,
+    'card': card,
+    'os': os
+})
